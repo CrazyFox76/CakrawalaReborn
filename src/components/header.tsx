8eac97dc885096366be7817c520b9ceb067c85a9
CrazyFox76 <crazyfox76@users.noreply.github.com>
2026-06-08 15:35:47 +0700

    test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
             <span className="text-sm font-bold text-white">CE</span>
           </div>
           <span className="text-lg font-bold text-primary">
-            Cakar <span className="text-accent">EduCentre</span>
+            Cakrawala <span className="text-accent">EduCentre</span>
           </span>
         </a>
 
