842b744964bd6f66e5f46bd3943fb47532cee14f
unknown <aditpratama77788@gmail.com>
2026-06-08 17:26:02 +0700

    lihat selengkapnya

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ThemeProvider from "@/components/theme-provider";
 import "./globals.css";
 
 const inter = Inter({
@@ -18,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" className={`${inter.className} scroll-smooth`}>
-      <body className="min-h-dvh flex flex-col bg-white text-zinc-800 antialiased">
-        {children}
+    <html lang="id" className={`${inter.className} scroll-smooth`} suppressHydrationWarning>
+      <body className="min-h-dvh flex flex-col bg-white text-zinc-800 antialiased dark:bg-zinc-950 dark:text-zinc-200">
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
