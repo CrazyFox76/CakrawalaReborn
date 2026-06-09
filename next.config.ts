006a65c7906ec50d57e560934feee5396ce82997
CrazyFox76 <crazyfox76@users.noreply.github.com>
2026-06-09 13:30:25 +0700

    chore: remove basePath for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/CakrawalaReborn",
   images: { unoptimized: true },
-  trailingSlash: true,
 };
 
 export default nextConfig;
